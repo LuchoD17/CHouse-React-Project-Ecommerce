f3fe8a977a6961ef9392bbd09b80254b8cf53f4a
luisdopico17@gmail.com <luisdopico17@gmail.com>
2021-11-22 23:18:01 -0300

    Agregado File ItemDetailContainer

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import Item from './Item';
-
-// const Item = ({ item }) => {
-//     return 
-//     <>
-//         <div className="container">
-//         <div className="card">
-//         <img src={item.img} className="cardImg" alt="producto"/>
-//         <div className="cardData">
-//         <h1 className="cardTitle">{item.name}</h1>
-//         <p className="cardDescription">{item.description}</p>
-//         <span className="cardPrice">${item.price}</span>
-//         <a href="#" className="cardButton">More Products</a>
-//         </div>
-//         </div>    
-//     </div>
-//     </>
-
-// }
-
-// export default Item
-
 import React from "react";
 import './Item.css'
 
